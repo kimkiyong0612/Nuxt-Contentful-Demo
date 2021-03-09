7bfe8767e0fe62e6a25de2f1ff2201259d512f94
kiyon.kimu <kiyon.kimu@northdetail.co.jp>
2021-03-09 16:08:07 +0900

    fix: deploy用にcross-env設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,25 @@
-const config = require('./.contentful.json')
+const environment = process.env.NODE_ENV || 'development';
+let envSet ={};
+
+if(environment ==='production'){
+  envSet ={
+    CTF_SPACE_ID: process.env.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: process.env.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID
+  }
+}else{
+  const config = require('./.contentful.json')
+  envSet ={
+      CTF_SPACE_ID: config.CTF_SPACE_ID,
+      CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+      CTF_PERSON_ID: config.CTF_PERSON_ID,
+      CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
+  }
+}
 
 module.exports = {
-  env: {
-    CTF_SPACE_ID: config.CTF_SPACE_ID,
-    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
-    CTF_PERSON_ID: config.CTF_PERSON_ID,
-    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
-  },
+  env: envSet,
   /*
   ** Headers of the page
   */
